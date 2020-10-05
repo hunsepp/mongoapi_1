f6bc588ae4cc5b9f2bcbd3d45cd9b3927bdaf59e
hunsepp <hunhand1205@gmail.com>
2020-10-05 12:20:07 +0900

    mongoose step1

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 var express = require('express')
-
-var app = express()
-
 var port = 8080;
-app.listen(port, function(){
-    console.log(`server is starting at http://localhost:${port}`)
+var app = express()
+var mongoose = require('mongoose')
+
+app.use(express.urlencoded({extended:false}))
+
+mongoose.connect('mongodb+srv://root:1234@cluster0.oplod.mongodb.net/mydb?retryWrites=true&w=majority', { 
+    useNewUrlParser: true , 
+    useUnifiedTopology: true 
+    })
+
+
+
+app.get('/',function(request,response){
+    // console.log(request)
+    response.send("hello world.!!!")
 })
 
-app.get('/',function(request){
-    console.log(request.IncomingMessage)
-    // response.send("hello world.")
+app.listen(port, function(){
+    console.log(`server is starting at http://localhost:${port}`)
 })
